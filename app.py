d94d228265f92a4e141987d34e52bd58e492723c
vishnia <vishni-a@ukr.net>
2023-08-25 15:43:21 +0300

    Fooh

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, CI/CD!"
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
